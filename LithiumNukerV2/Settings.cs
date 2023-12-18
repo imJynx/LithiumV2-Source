6dd191c48a5a2b2ef8be898598d95b332e865993
Jynx <149867187+imJynx@users.noreply.github.com>
2023-12-19 03:14:44 +1100

    Update Settings.cs

--- LithiumNukerV2/Settings.cs
+++ LithiumNukerV2/Settings.cs
@@ -31,7 +31,7 @@ namespace LithiumNukerV2
         public static long? GuildId = null;
         public static int Threads = 40;
         public static int ConnectionLimit = 25; // 10 concurrent connections
-        public static readonly string WebhookName = "jynx x anti runs you nigger";
+        public static readonly string WebhookName = "PUT WEBHOOK NAME HERE";
         public static readonly string AvatarUrl = "https://camo.githubusercontent.com/450b75468a748fbd8e4b3116c378cc9cfdcadd8b0b0e676821a6f873fcb85f53/68747470733a2f2f7665726c6f782e63632f7261772f535865475144";
     }
 }
